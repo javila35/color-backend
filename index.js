90b94987fabbc2dc663f7f5e168a2e31cfbdf64d
javila35 <josepharthuravila@gmail.com>
2021-04-24 10:33:01 -0700

    Use query params as route Remove hardcoded routes

--- index.js
+++ index.js
@@ -5,52 +5,40 @@ const routes = require("./routes.js");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+const colorOptions = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "purple",
+  "brown",
+  "gray",
+];
+
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json(routes)
+  res.json(routes);
 });
 
 app.get("/all", (req, res, next) => {
-    res.json(colors.all);
+  res.json(colors.all);
 });
 
-app.get("/red", (req, res) => {
-    res.json(colors.red);
-});
-
-app.get("/orange", (req, res) => {
-    res.json(colors.orange);
-});
-
-app.get("/yellow", (req, res) => {
-    res.json(colors.yellow);
-});
-
-app.get("/green", (req, res) => {
-    res.json(colors.green);
-});
-
-app.get("/blue", (req, res) => {
-    res.json(colors.blue);
-});
-
-app.get("/purple", (req, res) => {
-    res.json(colors.purple);
-});
-
-app.get("/brown", (req, res) => {
-    res.json(colors.brown);
-});
-
-app.get("/gray", (req, res) => {
-    res.json(colors.gray);
+app.get("/colors/:color", (req, res) => {
+  const colorParam = req.params.color;
+  if (colorOptions.includes(colorParam)) {
+    res.json(colors[colorParam]);
+  } else {
+    res.json("Invalid color route");
+  }
 });
 
 app.get("*", (req, res) => {
-    res.json("Invalid route");
+  res.json("Invalid route");
 });
 
-app.listen(PORT , () => {
-    console.log(`Server @ ${ PORT }`);
+app.listen(PORT, () => {
+  console.log(`Server @ ${PORT}`);
 });
